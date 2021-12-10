5506d11894ac69b32387c29dbec2079fc53698df
Ilhom5355 <ilhom575355@gmail.com>
2021-12-10 15:05:56 +0500

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  static final String id = "home_page";
+
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HereWeGo project"),
+      ),
+      body: Center(
+        child: Text("Hello Flutter"),
+      ),
+    );
+  }
+}
